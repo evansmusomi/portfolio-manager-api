39ff002187f4109f34bf36c8f31b5789f6c5eab8
Evans Musomi <evans.musomi@gmail.com>
2017-07-05 08:01:52 +0300

    Removes template filter for senior candidates

--- app.py
+++ app.py
@@ -15,26 +15,5 @@ init_api_routes(app)
 init_website_routes(app)
 init_error_handlers(app)
 
-# template filters
-
-
-@app.template_filter('senior_candidate')
-def senior_candidate(candidates):
-    """ Returns list of senior candidates (>= 5 years experience) """
-
-    result = []
-    for candidate in candidates:
-        for experience in candidate['experience']:
-            if experience['years'] >= 5:
-                result.append({
-                    'first_name': candidate['first_name'],
-                    'last_name': candidate['last_name'],
-                    'years': experience['years'],
-                    'domain': experience['domain']
-                })
-                break
-    return result
-
-
 if __name__ == "__main__":
     app.run(debug=True)
